d7bae843fb53f96a7fcab540d4810e20a4bc323a
zye-yint <zayaryeyint26@gmail.com>
2025-06-24 16:52:28 +0700

    zy

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -3,7 +3,7 @@ second_num = int(input("Enter the second number"))
 multiply = first_num * second_num
 if multiply > 0: 
    print(first_num , " * " ,second_num , " = " , multiply , "\nThe result is positive")
-if multiply < 0: 
+elif multiply < 0: 
    print( first_num , " * " , second_num , " = " , multiply , "\nThe result is negative")
-if multiply == 0: 
+else: 
   print( first_num , " * " , second_num , " = " , multiply ,"\nThe result is both positive nad negative")

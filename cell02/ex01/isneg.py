a3695f6be5c21a476807f0a9809e23783b592d34
zye-yint <zayaryeyint26@gmail.com>
2025-06-24 16:49:18 +0700

    zy

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,7 +1,7 @@
 number = int(input ("Enter a number: "))
 if number == 0:
-    print ("This number is both positive and negative")
-if number > 0:
-    print ("This number is positive ")
+    print ("This number is both positive and negative.")
+elif number > 0:
+    print ("This number is positive. ")
 if number < 0:
-  print("This number is negative")
+  print("This number is negative. ")

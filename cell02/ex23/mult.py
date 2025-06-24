22e298503f858ea0dbb4faa96335fa16ce1d9065
zye-yint <zayaryeyint26@gmail.com>
2025-06-24 14:05:55 +0700

    Update mult.py

--- cell02/ex23/mult.py
+++ cell02/ex23/mult.py
@@ -1,1 +1,9 @@
-
+first_num = int(input("Enter the first number"))
+second_num = int(input("Enter the second number"))
+multiply = first_num * second_num
+if multiply > 0: 
+   print(first_num , " * " ,second_num , " = " , multiply , "\nThe result is positive")
+if multiply < 0: 
+   print( first_num , " * " , second_num , " = " , multiply , "\nThe result is negative")
+if multiply == 0: 
+  print( first_num , " * " , second_num , " = " , multiply ,"\nThe result is both positive nad negative")

20e6c087630700f9a22997428cb86ca7ea0fca77
zye-yint <zayaryeyint26@gmail.com>
2025-06-24 12:35:02 +0700

    Update isneg.py

--- cell02/ex21/isneg.py
+++ cell02/ex21/isneg.py
@@ -1,1 +1,7 @@
-
+number = int(input ("Enter a number: "))
+if number == 0:
+    print ("This number is both positive and negative")
+if number > 0:
+    print ("This number is positive ")
+if number < 0:
+  print("This number is negative")

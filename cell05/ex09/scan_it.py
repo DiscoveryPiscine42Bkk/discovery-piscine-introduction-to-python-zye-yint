ba4dec9ec36c972a6235325fbaaa5989d75a13ca
zye-yint <zayaryeyint26@gmail.com>
2025-06-25 16:38:24 +0700

    Update scan_it.py

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,1 +1,9 @@
-
+#!/usr/bin/env python3
+import sys
+import re
+user = len(sys.argv)
+if user != 3:
+    print("none")
+else:
+    search = re.findall(sys.argv[1] , sys.argv[2])
+    print(len(search))

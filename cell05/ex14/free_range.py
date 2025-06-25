ce2a003478f459441d8d990e0525dc45e6bf28e2
zye-yint <zayaryeyint26@gmail.com>
2025-06-25 16:58:47 +0700

    Update free_range.py

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -1,1 +1,10 @@
-
+#!/usr/bin/env python3
+import sys
+a = len(sys.argv)
+if a != 3:
+print("none")
+else:
+start = int(sys.argv[1])
+end = int(sys.argv[2])
+arr = [j for j in range(start , end + 1)]
+print(arr)

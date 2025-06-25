4b21095796c002b06472c144e76f1198ff61f7d6
zye-yint <zayaryeyint26@gmail.com>
2025-06-25 17:02:16 +0700

    Update play_with_arrays.py

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-arr1 = [2,8,9,48,8,22,-12,2]
-arr2 = [(i+ 2)  for i in arr1 if i > 5]
-print(arr1)
-print(arr2)
+array_1 = [2,8,9,48,8,22,-12,2]
+array_2 = [(i+ 2)  for i in array_1 if i > 5]
+print(array_1)
+print(array_2)

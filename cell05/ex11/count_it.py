a2390ac8739035d07b50ec112f891cdb050ee759
zye-yint <zayaryeyint26@gmail.com>
2025-06-25 16:43:15 +0700

    Update count_it.py

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -1,1 +1,9 @@
-
+#!/usr/bin/env python3
+import sys
+user = len(sys.argv)
+if user == 1:
+print("none")
+else:
+print("Parameters: " + str(len(sys.argv) - 1))
+for a in range(1, len(sys.argv)):
+print(f"{sys.argv[a]}: {len(sys.argv[a])}")

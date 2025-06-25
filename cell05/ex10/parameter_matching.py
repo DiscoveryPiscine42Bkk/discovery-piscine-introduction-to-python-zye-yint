b386ee8378eb9abedf21a8a5a6969c0432e1c249
zye-yint <zayaryeyint26@gmail.com>
2025-06-25 16:40:46 +0700

    Update parameter_matching.py

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,1 +1,11 @@
-
+#!/usr/bin/env python3
+import sys
+user = len(sys.argv)
+if user != 2:
+print("none")
+else:
+string = input("What was the parameter? ")
+if string == sys.argv[1]:
+print("Good job!")
+else:
+print("Nope, sorry...")

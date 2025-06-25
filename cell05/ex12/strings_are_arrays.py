bbe537ddab0f8af21ceb664e56e288a08b2cec65
zye-yint <zayaryeyint26@gmail.com>
2025-06-25 16:46:49 +0700

    Update strings_are_arrays.py

--- cell05/ex12/strings_are_arrays.py
+++ cell05/ex12/strings_are_arrays.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python3
+count = 0 
+import sys
+a = len(sys.argv)
+if a != 2:
+print("none")
+else:
+for i in sys.argv[1]:
+if i == 'z':
+print('z', end = '')
+count+= 1
+if count == 0:
+print("none")
+else:
+print()
 

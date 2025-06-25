1fd8af6a3e97e0e1ed29f4632886bb7aacf54ea1
zye-yint <zayaryeyint26@gmail.com>
2025-06-25 11:10:59 +0700

    Update calculator.py

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,1 +1,11 @@
-
+firstnum = int(input("Enter first number: "))
+secondnum = int(input("Enter second number: "))
+print("Thank you!")
+sum = firstnum+secondnum
+minus = firstnum-secondnum
+multiply = firstnum*secondnum
+divide = firstnum/secondnum
+print(f"{firstnum}+{secondnum}={sum}")
+print(f"{firstnum}-{secondnum}={minus}")
+print(f"{firstnum}/{secondnum}={divide}")
+print(f"{firstnum}*{secondnum}={multiply}")

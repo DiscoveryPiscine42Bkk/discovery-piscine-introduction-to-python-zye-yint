dea8c39c93db89799171e2db7580b7d4b0c44c44
zye-yint <zayaryeyint26@gmail.com>
2025-06-25 16:55:38 +0700

    Update append_it.py

--- cell05/ex13/append_it.py
+++ cell05/ex13/append_it.py
@@ -1,1 +1,12 @@
-
+#!/usr/bin/env python3
+import sys
+import re
+user = len(sys.argv)
+if user == 2:
+print('none')
+else:
+for a in range(1 , len(sys.argv)):
+if len(re.findall("ism" , sys.argv[a])) > 0:
+pass
+else:
+print(sys.argv[a] + "ism")

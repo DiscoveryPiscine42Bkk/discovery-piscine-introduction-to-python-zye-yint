e957571cb6c89a6851edc2601863e66cbc715de4
zye-yint <zayaryeyint26@gmail.com>
2025-06-26 15:22:04 +0700

    Update greeting_for_all.py

--- cell06/ex03/greeting_for_all.py
+++ cell06/ex03/greeting_for_all.py
@@ -1,1 +1,10 @@
-
+#!/usr/bin/env python3
+def greetings(name="noble stranger"):
+  if isinstance(name,str):
+    print(f"Heloo,{name}.")
+  else:
+    print("Error! It was not a name.")
+greetings('Alexandra')
+greetings('wil')
+greetings()
+greetings(42)

0414480c62ad2b3a8cea30b71d66d125291ea362
zye-yint <zayaryeyint26@gmail.com>
2025-06-26 15:35:23 +0700

    Update scope_that.py

--- cell06/ex05/scope_that.py
+++ cell06/ex05/scope_that.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+def add_one(x):
+    return x + 1
 
+# Main program
+value = 5
+print(value)         # Print original value
+add_one(value)       # Call function but don't store result
+print(value)         # Still original value

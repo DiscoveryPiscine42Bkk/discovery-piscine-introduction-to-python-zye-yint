afbe5626886ab427814ad7be7f111edc2ac3c690
zye-yint <zayaryeyint26@gmail.com>
2025-06-26 15:30:29 +0700

    Update methods_everywher.py

--- cell06/ex04/methods_everywher.py
+++ cell06/ex04/methods_everywher.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python3
+import sys
 
+def shrink(s):
+    print(s[:8])
+
+def enlarge(s):
+    print(s + 'Z' * (8 - len(s)))
+
+def main():
+    args = sys.argv[1:]
+    if len(args) < 1:
+        print("none")
+    else:
+        for word in args:
+            if len(word) > 8:
+                shrink(word)
+            elif len(word) < 8:
+                enlarge(word)
+            else:
+                print(word)
+
+if __name__ == "__main__":
+    main()

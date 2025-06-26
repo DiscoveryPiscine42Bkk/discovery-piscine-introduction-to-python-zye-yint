3431614955c8379c3b5766bcceee9179b053e2f8
zye-yint <zayaryeyint26@gmail.com>
2025-06-26 23:15:11 +0700

    Update main.py

--- rush00/ex00/main.py
+++ rush00/ex00/main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-from checkmate import checkmate
+from checkmate import is_king_attacked
+import textwrap
 
 def main():
-    board = """\
-R...
-.K..
-..P.
-...."""
-    checkmate(board)
+    board_input = textwrap.dedent("""
+        ....
+        .K..
+        ..P.
+        ....
+    """)
+    is_king_attacked(board_input)
 
 if __name__ == "__main__":
     main()
